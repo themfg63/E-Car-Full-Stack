142bcf4d43820443f59c2d8aaa57e21fa1c4f2a7
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-08-15 13:46:16 +0300

    project created

---
+++ src/main/java/com/TheMFG/E_Car/ECarApplication.java
@@ -0,0 +1,13 @@
+package com.TheMFG.E_Car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheMFG/E_Car/ECarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheMFG.E_Car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
