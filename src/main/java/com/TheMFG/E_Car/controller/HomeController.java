fc2e01139eef0c49ca5c5e67c7041456c84ec5b9
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-08-15 14:47:18 +0300

    login and index pages created

---
+++ src/main/java/com/TheMFG/E_Car/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.TheMFG.E_Car.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/signin")
+    public String login(){
+        return "login";
+    }
+}
